0e3f35271be07d1145c4f6badc0275a0aad8266e
Houngue Yaovi <hfyaovi@gmail.com>
2019-12-17 14:46:12 +0300

    mixed minor updates

--- footer.php
+++ footer.php
@@ -1,33 +1,24 @@
 <?php wp_footer() ?>
 <footer class="footer">
-      <div class="container text-center py-3 py-md-5">
-        <img src="<?php bloginfo('template_directory');?>/images/logo-white.svg" class="img-fluid" alt="" />
-        <p class="text-white">
-          сделано в России (посуточная аренда жилья с бесключевым доступом с
-          гарантией заселения)
-        </p>
-        <div class="d-flex justify-content-center">
-          <a href="#" target="_blank" rel="noopener noreferrer"
-            ><img src="<?php bloginfo('template_directory');?>/images/vk.svg" class="img-fluid mx-2 mx-md-4" alt="" />
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer"
-            ><img
-              src="<?php bloginfo('template_directory');?>/images/insta.svg"
-              class="img-fluid mx-2 mx-md-4"
-              alt=""
-            />
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer"
-            ><img
-              src="<?php bloginfo('template_directory');?>/images/youtube.svg"
-              class="img-fluid mx-2 mx-md-4"
-              alt=""
-            />
-          </a>
-        </div>
-      </div>
-    </footer>
-    <!-- <script src="https://maps.api.2gis.ru/2.0/loader.js?pkg=full"></script> -->
+  <div class="container text-center py-3 py-md-5">
+    <img src="<?php bloginfo('template_directory');?>/images/logo-white.svg" class="img-fluid" alt="" />
+    <p class="text-white">
+      сделано в России (Система контроля доступа для квартир в аренду)
+    </p>
+    <div class="d-flex justify-content-center">
+      <a href="#" target="_blank" rel="noopener noreferrer"><img
+          src="<?php bloginfo('template_directory');?>/images/vk.svg" class="img-fluid mx-2 mx-md-4" alt="" />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer"><img
+          src="<?php bloginfo('template_directory');?>/images/insta.svg" class="img-fluid mx-2 mx-md-4" alt="" />
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer"><img
+          src="<?php bloginfo('template_directory');?>/images/youtube.svg" class="img-fluid mx-2 mx-md-4" alt="" />
+      </a>
+    </div>
+  </div>
+</footer>
 
 </body>
+
 </html>
