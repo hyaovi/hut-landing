a4405f7a37ff780ea75b74801311cfbd6289ec13
Houngue Yaovi <hfyaovi@gmail.com>
2019-12-17 12:40:04 +0300

    remove yandex metric from header.php

--- header.php
+++ header.php
@@ -7,20 +7,7 @@
     <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_directory');?>/images/favicon.ico" />
     <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_directory');?>/images/favicon-logo.png" />
     <meta name="theme-color" content="#ffffff" />
-    <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(56606980, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/56606980" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
     <title>Hut Sharing</title>
     <?php  wp_head(); ?>
   </head>
